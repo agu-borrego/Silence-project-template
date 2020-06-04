d838a639130420fadcb0bde3c3edbeafa14fcca1
Agustín Borrego <borrego@us.es>
2020-06-04 11:04:13 +0200

    Refactored settings

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
-DEBUG_ENABLED = False
-HTTP_PORT = 9999
-API_PREFIX = "/api"
+###############################################################################
+# Project-specific settings
+###############################################################################
 
-RUN_API = True
-RUN_WEB = True
+# Shows debug messages while Silence is running
+DEBUG_ENABLED = True
 
+# Database connection details
 DB_CONN = {
     "host": "localhost",
     "port": 3306,
@@ -13,13 +14,22 @@ DB_CONN = {
     "database": "app",
 }
 
+# The sequence of SQL scripts located in the sql/ folder that must
+# be ran when the 'silence createdb' command is issued
+SQL_SCRIPTS = [
+    "create_tables.sql",
+    "populate_database.sql"
+]
+
+# The port in which the API and the web server will be deployed
+HTTP_PORT = 8080
+
+# The URL prefix for all API endpoints
+API_PREFIX = "/api"
+
+# Table and fields that are used for both login and register
 USER_AUTH_DATA = {
     "table": "Employees",
     "identifier": "email",
     "password": "password",
 }
-
-SQL_SCRIPTS = [
-    "create_tables.sql",
-    "populate_database.sql"
-]
